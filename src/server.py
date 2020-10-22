0a56d890b756aae51dbfff6220f2f508c8f4aec4
xXHachimanXx <globaldep90@gmail.com>
2020-10-21 22:33:19 -0300

    Criando classes úteis.

--- src/server.py
+++ src/server.py
@@ -1,21 +1,44 @@
 import socket
 
-# criando um objeto socket
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+class Fuel:
+    def __init__(self, fuelType, fuelPrice):
+        self.fuelType = fuelType
+        self.fuelPrice = int(fuelPrice * 1000)
+    
+    def __str__(self):
+        return (self.fuelType + ' ' + self.fuelPrice)
 
-# pegar nome da maquina local
-host = socket.gethostname()
-port = 3333
+class GasStation:
+    def __init__(self, fuels):
+        self.fuels = fuels
+    
 
-serversocket.bind((host, port))
+def upServer():    
+    serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    # pegar nome da maquina local
+    host = socket.gethostname()
+    port = 3333
 
-serversocket.listen(5)
+    serversocket.bind((host, port))
 
-while True:
-    clientsocket, addr = serversocket.accept()
 
-    print("Got a connection from %s" % str(addr))
+def main():
+    listOfGasStations = []
+    
+    with open('gas_stations.txt', 'r') as gasStations:
+        for gasStation in gasStations:
+            gasStationDetais = gasStation.split(' ')
+            
+            
+        
+    # serversocket.listen(5)
 
-    msg = 'Thanks for connect' + '\r\n'
-    clientsocket.send(msg.encode('ascii'))
-    clientsocket.close()
+    # while True:
+    #     clientsocket, addr = serversocket.accept()
+
+    #     print("Got a connection from %s" % str(addr))
+
+    #     msg = 'Thanks for connect' + '\r\n'
+    #     clientsocket.send(msg.encode('ascii'))
+    #     clientsocket.close()
